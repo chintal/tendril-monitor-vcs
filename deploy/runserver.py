76a19ef48a853967b83c06a81a59b267909b8247
Chintalagiri Shashank <shashank@chintal.in>
2017-03-28 03:15:35 +0530

    Initial superstructure commit.

---
+++ deploy/runserver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Copyright (C) 2015 Chintalagiri Shashank
+# Released under the MIT license.
+
+"""
+
+Simple Deployment Example
+-------------------------
+
+"""
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == '__main__':
+    pass
---
+++ vcs_monitor/worker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Copyright (C) 2015 Chintalagiri Shashank
+# Released under the MIT license.
+
+"""
+Sets up a :class:`twisted.web.resource.Resource` hierarchy containing the
+available pre-assembled data elements.
+"""
+
+
+def get_service():
+    pass
